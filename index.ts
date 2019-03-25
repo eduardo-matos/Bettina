65ce29b5207615a500fbe12d3982e5b5451e68a4
Eduardo Matos <eduardo.matos@redealumni.com>
2019-03-25 09:50:34 -0300

    Type functions

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
-export function milliseconds(value) {
+export function milliseconds(value: number): number {
   return value;
 }
 
-export function seconds(value) {
+export function seconds(value: number): number {
   return 1000 * value;
 }
 
-export function minutes(value) {
+export function minutes(value: number): number {
   return 1000 * 60 * value;
 }
